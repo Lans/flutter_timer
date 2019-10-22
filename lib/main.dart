e2a9e6970277f6dfc3784273d948ab0b73d44365
lans <wylans@163.com>
2019-10-22 15:26:15 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: '倒计时demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Timer _timer;
+
+  //倒计时数值
+  var countdownTime = 0;
+
+  //倒计时方法
+  startCountdown() {
+    //倒计时时间
+    countdownTime = 10;
+    final call = (timer) {
+      if (countdownTime < 1) {
+        _timer.cancel();
+      } else {
+        setState(() {
+          countdownTime -= 1;
+        });
+      }
+    };
+    _timer = Timer.periodic(Duration(seconds: 1), call);
+  }
+
+  String handleCodeAutoSizeText() {
+    if (countdownTime > 0) {
+      return "${countdownTime}s后重新获取";
+    } else
+      return "获取验证码";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              handleCodeAutoSizeText(),
+              style: TextStyle(fontSize: 25),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(80),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    child: MaterialButton(
+                      onPressed: () {
+                        if (countdownTime == 0) {
+                          startCountdown();
+                        }
+                      },
+                      child: Text("开始"),
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(""),
+                  ),
+                  Expanded(
+                    child: MaterialButton(
+                      onPressed: () {
+                        setState(() {
+                          countdownTime = 0;
+                        });
+                      },
+                      child: Text("结束"),
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_timer != null) {
+      _timer.cancel();
+    }
+  }
+}
